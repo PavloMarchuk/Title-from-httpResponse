741fbbdf42c410cba0816701be430f67dc10ac46
Pavlo Marchuk <pavlo_marchuk@hotmail.com>
2018-02-05 16:56:11 +0200

    1

--- WebUi/Startup.cs
+++ WebUi/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace WebUi
-{
+{//
     public class Startup
     {
         public Startup(IConfiguration configuration)
